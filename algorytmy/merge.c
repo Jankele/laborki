84cc72e338e6ee3a2db4981d61a625a966075654
janke <janke@janke.com>
2017-12-01 15:29:24 +0100

    add

--- algorytmy/merge.c
+++ algorytmy/merge.c
@@ -1,19 +1,64 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int merge(int tab[])
+void merge(int lewa[], int tab[], int prawa[], int lewa_dlugosc, int prawa_dlugosc)
 {
-
+    int lewa_licznik=0;
+    int prawa_licznik=0;
+    int tab_licznik=0;
+    while(lewa_licznik < lewa_dlugosc && prawa_licznik < prawa_dlugosc)
+    {
+        if(lewa[lewa_licznik] < prawa[prawa_licznik])
+            tab[tab_licznik++]=lewa[lewa_licznik++];
+        else
+            tab[tab_licznik++]=prawa[prawa_licznik++];
+    }
+    while(lewa_licznik < lewa_dlugosc)
+        tab[tab_licznik++] = lewa[lewa_licznik++];
+    while(prawa_licznik<prawa_dlugosc)
+        tab[tab_licznik++] = prawa[prawa_licznik++];
 }
 
-int dismerge(int tab[], int n)
+void sort(int tab[], int n)
 {
-    int srodek = n/2;
-
+    int lewa_dlugosc = n/2;
+    int prawa_dlugosc = n-lewa_dlugosc;
+    int lewa[lewa_dlugosc];
+    int prawa[prawa_dlugosc];
+    if(n<2)
+        return;
+    for(int i=0;i<lewa_dlugosc;i++)
+        lewa[i] = tab[i];
+    for(int i=lewa_dlugosc;i<n; i++)
+        prawa[i - lewa_dlugosc] = tab[i];
+    sort(lewa,lewa_dlugosc);
+    sort(prawa, prawa_dlugosc);
+    merge(lewa, tab, prawa, lewa_dlugosc, prawa_dlugosc);
 }
 
+void wyswietl(int tab[], int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
+}
+
+int wypelnij(int tab[])
+{
+    int n;
+    printf("Podaj dlugosc tablicy: ")
+    fgets(n);
+    for(int i)
+}
 int main()
 {
-    int tab[] = {2,5,6,3,1,7,9,8};
+    int tab[] = wypelnij(tab);
     int n = sizeof(tab) / sizeof(int);
-    printf("");
+    printf("Tablica przed sortowaniem:\n");
+    wyswietl(tab, n);
+    sort(tab,n);
+    wyswietl(tab,n);
+    return 0;
 }
