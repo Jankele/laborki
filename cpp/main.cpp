cd49bd84cdb977f51d72f5efacb91333390a030d
Lab User <user@wi.zut.pl>
2017-12-09 14:48:09 +0100

    add

---
+++ cpp/main.cpp
@@ -0,0 +1,10 @@
+#include <cstdlib>
+#include "file.hpp"
+
+using namespace std;
+
+int main()
+{
+	hello();
+	return 0;
+}
--- labkic3/program1.c
+++ labkic3/program1.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 
+int main()
+{
+	int liczba;
+	int potega;
+	FILE *file = fopen("plik1.txt", "r");
+	FILE *file2 = fopen("plik2.txt", "w");
+	while(file != EOF){
+	fscanf(file, "%d", &liczba);
+	potega = liczba*liczba;
+	fprintf(file2, "%d ", potega);}
+	fclose(file);
+	fclose(file2);
+	return 0; 
+}
