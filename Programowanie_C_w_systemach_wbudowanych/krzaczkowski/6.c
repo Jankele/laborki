ea586c614294138eff803968af15b187a2abdcb0
Jankels <jasio1991@gmail.com>
2017-12-30 15:32:27 +0100

    add

--- Programowanie_C_w_systemach_wbudowanych/krzaczkowski/6.c
+++ Programowanie_C_w_systemach_wbudowanych/krzaczkowski/6.c
@@ -50,16 +50,36 @@ int zad17(int **tab, int n, int m)
 	return max;
 }
 
+int **zad21(int **tab, int n, int m)
+{
+	int **temp, x, k;
+	temp = tab;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0, k=m-1;k>=0;j++, k--)
+		{
+			x = temp[i][k];
+			tab[i][j] = x;
+		}
+	}
+	return tab;
+}
 int main()
 {
 	int n=5, m=6;
-	int **tab1;
-	int **tab8;
+	int **tab1, **tab8, **tab21;
 	tab1 = zad1(n,m);
 	tab8 = zad8(tab8, n);
+	tab21 = zad1(n,m);
 	wyswietl(tab1, n, m);
 	printf("\n\n");
 	wyswietl(tab8, n, M);
+	tab1[0][0] = 123;
 	printf("\n%d\n", zad17(tab1, n, m));
+	printf("\n\n");
+	wyswietl(tab21, n, m);
+	tab21 = zad21(tab21, n, m);
+	printf("\n\n");
+	wyswietl(tab21, n, m);
 	return 0;
 }
