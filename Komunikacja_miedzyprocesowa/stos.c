12d272f9e57f21b2e4e48cc3153b12b39d849813
janke <janke@janke.com>
2018-01-12 13:15:15 +0100

    add

--- Komunikacja_miedzyprocesowa/stos.c
+++ Komunikacja_miedzyprocesowa/stos.c
@@ -1,14 +1,31 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <semaphore.h>
-#include <fcntl.h>
-#include <sys/wait.h>
-#include <sys/mman.h>
+/*
++++ Program zapisuje/odczytuje z tablicy statycznej, ktora reprezentuje stos.
++++ Stos jest wypełniany pseudolosowymi liczbami z zakresu 1-30.
++++ Po "zdjeciu" elementu ze stosu pod odpowiednim indeksem tablicy wstawiana jest liczba 0.
++++ Proces dziecka odpowiada za zapis, natomiast proces rodzica za odczyt.
++++ Semafor ktory obsluguje synchronizacje po stronie producenta zainicjalizowany jest z wartoscia 
+	wieksza od zera, dzieki czemu mamy gwarancje, ze odczyt nigdy nie wykona sie pierwszy.
++++ Dzieki MAP_ANONYMOUS oraz -1 w miejscu deskryptora pliku nie potrzeba otwierac pliku ani ustawiac jego rozmiaru.
++++ Aby umożliwić wypisanie liczby która została wrzucona na stos, obie funkcje sa typu int.
 
-#define N 5
 
-unsigned short iterator = 0;
++++ Procesy przed końcem wykonania przejścia w petli zasypiaja, 
+	aby lepiej zobrazować zapis/odczyt w toku wykonywania programu.
++++ Wartości usleep różnią się od siebie w celu uniknięcia fałszywej synchronizacji,
+	ktora pojawia sie czesto podczas gdy sen trwa taki sam czas
++++ Aby zauważyć działanie warunków w funkcjach push_pop oraz pop_front zakomentowałem semafory.
++++ Po odkomentowaniu semaforow procesy zostana zsynchronizowane,
+	dzieki czemu nigdy nie dojdzie do sytuacji w ktorej stos moze byc pusty lub przepelniony.
+*/
+#include <stdio.h> //printf
+#include <unistd.h> //fork
+#include <stdlib.h> //exit, rand
+#include <semaphore.h> //semafory
+#include <sys/wait.h> // wait
+#include <sys/mman.h> //dyrektywy na potrzeby pamieci wspoldzielonej
+
+#define N 5 //ilosc elementow w tablicy
+#define PETLA 40 //ilosc przejsc w petlach for
 
 int push_pop(int *node);
 int pop_front(int *node);
@@ -17,18 +34,13 @@ int main()
 {
 	pid_t pid;
 	int status, wynik;
-	/*int shm_tab = shm_open("/shm_tab", O_CREAT | O_EXCL | O_RDWR, 0600);
-	int shm_pelny = shm_open("/shm_pelny", O_CREAT | O_EXCL | O_RDWR, 0600);
-	int shm_pusty = shm_open("/shm_pusty", O_CREAT | O_EXCL | O_RDWR, 0600);
-	ftruncate(shm_tab, sizeof(int)*N);
-	ftruncate(shm_pelny, sizeof(sem_t));
-	ftruncate(shm_pusty, sizeof(sem_t));*/
 	int *stos = mmap(NULL, sizeof(int)*N, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-	sem_t *pelny = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-	sem_t *pusty = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
-
-	sem_init(pusty,  1, 1);
-	sem_init(pelny,  1, 0);
+	/*sem_t *pelny = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);
+	sem_t *pusty = mmap(NULL, sizeof(sem_t), PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_SHARED, -1, 0);*/
+	for(int i=0;i<=N;i++)
+		stos[i]=0;
+	/*sem_init(pusty,  1, 1);
+	sem_init(pelny,  1, 0);*/
 
 	if((pid = fork()) == -1)
 	{
@@ -37,81 +49,73 @@ int main()
 	}
 	else if(pid == 0) //dziecko - producent
 	{
-		for(int i=0;i<10;i++)
+		for(int i=0;i<PETLA;i++)
 		{
-				sem_wait(pusty);
+				//sem_wait(pusty);
 					wynik = push_pop(stos);
 					if(wynik == -1)
-						puts("Lista jest przepełniona!");
+						puts("Stos jest przepełniony!");
 					else
-						printf("Producent wrzucil na stos %d\n", wynik);
-				sem_post(pelny);
-				usleep(150000);
+						printf("PRODUCENT WRZUCIL NA STOS %d\n", wynik);
+				//sem_post(pelny);
+				usleep(19000);
 		}
-				for(int i=N;i<=N;i++)
-			printf("%d ", stos[i]);
-		puts("");
-		sem_close(pusty);
-		sem_close(pelny);
-		/*close(shm_tab);
-		close(shm_pelny);
-		close(shm_pusty);*/
-		return 0;
+		/*sem_close(pusty);
+		sem_close(pelny);*/
+		puts("Dziecko zakonczylo swoja prace");
+		exit(EXIT_SUCCESS);
 	}
 	else // rodzic - konsument
 	{
-		for(int i=0;i<10;i++)
+		for(int i=0;i<PETLA;i++)
 		{
-			sem_wait(pelny);
+		//	sem_wait(pelny);
 				wynik = pop_front(stos);
 				if(wynik == -1)
-					puts("Lista jest pusta!");
+					puts("Stos jest pusty!");
 				else
-					printf("-----------------------------Konsument sciagnal ze stosu %d | iterator = %d\n", wynik, iterator);
-			sem_post(pusty);
-
+					printf("----------------------KONSUMENT SCIAGNAL ZE STOSU %d\n", wynik);
+		//	sem_post(pusty);
+				usleep(60000);
 		}
-		wait(&status);
-		sem_close(pusty);
+		puts("Rodzic zakonczyl swoja prace");
+		wait(&status); // czekaj na zakonczenie procesu dziecka
+		puts("Dziecko zakonczyło sie z sukcesem");
+		/*sem_close(pusty);
 		sem_close(pelny);
-		/*close(shm_tab);
-		close(shm_pelny);
-		close(shm_pusty);*/
 		sem_destroy(pusty);
-		sem_destroy(pelny);
-		/*shm_unlink("/shm_tab");
-		shm_unlink("/shm_pelny");
-		shm_unlink("/shm_pusty");*/
-		return 0;
+		sem_destroy(pelny);*/
+		exit(EXIT_SUCCESS);
 	}
 }
 
 int push_pop(int *node)
 {
-    if(iterator > N)
-    {
-        return -1;
-    }
-    else
-    {
-    	int a = rand()%30 + 1;
-        node[iterator-1] = a;
-        iterator++;
-        //return node[iterator-1];
-        return a;
-    }
+	unsigned short a = rand()%30 + 1; //losowanie z przedzialu <1-31>
+	unsigned short iterator = 0;
+	for(int i=0;i<N;i++) //zlicza ilosc elementow na stosie (czyli niezerowych)
+	{
+		if(node[i] > 0)
+			iterator++;
+	}
+	if(iterator == N) // jesli liczba elementow na stosie rowna sie rozmiarowi tablicy, zwroc -1
+		return -1;
+	node[iterator] = a;
+	return node[iterator];
 }
 
 int pop_front(int *node)
 {
-    if(iterator < 1)
-    {
-        return -1;
+	unsigned short wynik;
+	unsigned short iterator = 0;
+	for(int i=0;i<N;i++)
+	{
+		if(node[i] > 0) //zlicza ilosc elementow na stosie (czyli niezerowych)
+			iterator++;
     }
-    else
-    {
-        node[iterator-1] = 0;
-        iterator--;
-        return node[iterator];
-    }
+    if(iterator == 0) // jesli liczba elementow na stosie rowna sie 0, zwroc -1
+    	return -1;
+    wynik = node[iterator-1]; //zwraca ostatni niezerowy element oraz go zwraca
+    node[iterator-1] = 0;
+    return wynik;
 }
