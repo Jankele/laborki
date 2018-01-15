af06ac97e48449513a0b087cf7c7188903920a74
janke <janke@janke.com>
2018-01-15 15:28:16 +0100

    add

--- Dynamiczne_zarzadzanie_pamiecia_i_algorytmy/Kodowanie_Huffmana/kodowanie_huffmana.c
+++ Dynamiczne_zarzadzanie_pamiecia_i_algorytmy/Kodowanie_Huffmana/kodowanie_huffmana.c
@@ -18,6 +18,15 @@ typedef struct node
 	struct node *prawa;
 } node;
 
+typedef struct lista
+{
+	struct node *lisc;
+	struct lista *next; 
+} lista;
+
+node *wezel = NULL;
+lista *glowa = NULL; 
+
 char *czytaj_plik(const char *plik)
 {	
 	char *tekst = NULL;
@@ -43,9 +52,48 @@ char *czytaj_plik(const char *plik)
 	return tekst;
 }
 
-void zlicz_liczbe_wystapien(char znak)
+void stworz_lisc(char znak)
 {
-	
+	lista *tmp = NULL;
+	int czy_znak_wystapil = 0;
+	//ALOKACJA PAMIECI DLA TYMCZASOWEGO ELEMENTU STOSU
+	if(!(tmp = malloc(sizeof(lista))))
+	{	
+		perror("Blad podczas alokowania pamieci");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		tmp->next = NULL;
+		tmp->lisc->znak = znak;
+		tmp->lisc->lewa = NULL;
+		tmp->lisc->prawa = NULL; 
+	}
+	//ZLICZENIE ZNAKOW ORAZ ZMIANA WSKAZNIKA GLOWY NA TYMCZASOWY ELEMENT
+	if(glowa == NULL)
+	{
+		tmp->lisc->ilosc = 1;
+		glowa = tmp;
+	}
+	else
+	{
+		tmp = glowa;
+		while(tmp)
+		{
+			if(tmp->lisc->znak == znak)
+			{
+				tmp->lisc->ilosc += 1;
+				czy_znak_wystapil++;
+			}
+			tmp = tmp->next;	
+		}
+		if(czy_znak_wystapil == 0)
+		{
+			tmp->lisc->ilosc = 1;
+			tmp->next = glowa;
+			glowa = tmp;
+		}
+	}
 }
 
 void stworz_drzewo_HF()
@@ -56,9 +104,16 @@ void usun_drzewo_HF()
 {
 
 }
-void wyswietl()
+void wyswietl_powtorzenia(lista *node)
 {
-
+	lista *tmp = NULL;
+	tmp = node;
+	while(tmp)
+	{
+		printf("Znak %c : %d\n", tmp->lisc->znak, tmp->lisc->ilosc);
+		tmp = tmp->next;
+	}
+	free(tmp);
 }
 void kodowanie_HF()
 {
@@ -72,5 +127,8 @@ int main()
 	printf("%s\n", tekst);
 	int dlugosc = strlen(tekst);
 	printf("%d\n", dlugosc);
+	for(int i=0;i<dlugosc;i++)
+		stworz_lisc(tekst[0]);
+	wyswietl_powtorzenia(glowa);
 	return EXIT_SUCCESS;
 }
