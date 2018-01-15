9bab48691bff79a9daaee7a6137a1dab6d75537a
Jankels <jasio1991@gmail.com>
2018-01-15 20:37:01 +0100

    add

--- Dynamiczne_zarzadzanie_pamiecia_i_algorytmy/Kodowanie_Huffmana/kodowanie_huffmana.c
+++ Dynamiczne_zarzadzanie_pamiecia_i_algorytmy/Kodowanie_Huffmana/kodowanie_huffmana.c
@@ -3,7 +3,12 @@
 	Jan Komorkiewicz
 --------------------------------------------------------
 
-Czytamy z pliku tekst.txt ktory musi znajdowac sie w tym samym katologu w ktorym uruchamiamy program
++++ Czytamy z pliku tekst.txt ktory musi znajdowac sie w tym samym katologu w ktorym uruchamiamy program.
++++ Adres glowy przekazywany jest do funkcji jako wartosc, dzieki czemu nie musze uzywac zmiennych globalnych.
++++ Program wczytuje tylko pierwsza linie pliku tekstowego.
++++ Funkcja wyswietlajaca ilosc wystapien danego znaku nie zapewnia obslugi wszystkich znakow specjalnych,
+	przez co znaki tj. "vertical tab" lub niektore znaki oprocz spacji i znaku EOL moga sie wyswietlac bez opisu.
++++ 
 */
 
 #include <stdlib.h> //malloc, etc.
@@ -24,8 +29,7 @@ typedef struct lista
 	struct lista *next; 
 } lista;
 
-node *wezel = NULL;
-lista *glowa = NULL; 
+
 
 char *czytaj_plik(const char *plik)
 {	
@@ -52,48 +56,46 @@ char *czytaj_plik(const char *plik)
 	return tekst;
 }
 
-void stworz_lisc(char znak)
+void stworz_glowe(char znak, lista **glowa)
 {
-	lista *tmp = NULL;
-	int czy_znak_wystapil = 0;
-	//ALOKACJA PAMIECI DLA TYMCZASOWEGO ELEMENTU STOSU
-	if(!(tmp = malloc(sizeof(lista))))
-	{	
-		perror("Blad podczas alokowania pamieci");
-		exit(EXIT_FAILURE);
-	}
-	else
-	{
-		tmp->next = NULL;
-		tmp->lisc->znak = znak;
-		tmp->lisc->lewa = NULL;
-		tmp->lisc->prawa = NULL; 
-	}
-	//ZLICZENIE ZNAKOW ORAZ ZMIANA WSKAZNIKA GLOWY NA TYMCZASOWY ELEMENT
-	if(glowa == NULL)
-	{
-		tmp->lisc->ilosc = 1;
-		glowa = tmp;
-	}
-	else
-	{
-		tmp = glowa;
-		while(tmp)
+	*glowa = malloc(sizeof(lista));
+	(*glowa)->lisc = malloc(sizeof(node));
+	(*glowa)->next = NULL;
+	(*glowa)->lisc->znak = znak;
+	(*glowa)->lisc->lewa = NULL;
+	(*glowa)->lisc->prawa = NULL;
+	(*glowa)->lisc->ilosc = 1;
+}
+
+void push_front_and_count(char znak, lista **glowa)
+{	
+	//FUNKCJA TWORZY
+		lista *tmp = NULL; 
+		tmp = *glowa;
+		int czy_znak_wystapil = 0;
+		while(tmp != NULL)
 		{
 			if(tmp->lisc->znak == znak)
 			{
 				tmp->lisc->ilosc += 1;
 				czy_znak_wystapil++;
+				break;//nie potrzebujemy dalej przechodzic
 			}
 			tmp = tmp->next;	
 		}
 		if(czy_znak_wystapil == 0)
 		{
-			tmp->lisc->ilosc = 1;
-			tmp->next = glowa;
-			glowa = tmp;
+		lista *tmp = NULL;
+		tmp = malloc(sizeof(lista));
+		tmp->lisc = malloc(sizeof(node));
+		tmp->next = NULL;
+		tmp->lisc->znak = znak;
+		tmp->lisc->lewa = NULL;
+		tmp->lisc->prawa = NULL;
+		tmp->lisc->ilosc = 1;
+		tmp->next = *glowa;
+		*glowa = tmp;
 		}
-	}
 }
 
 void stworz_drzewo_HF()
@@ -104,17 +106,23 @@ void usun_drzewo_HF()
 {
 
 }
-void wyswietl_powtorzenia(lista *node)
+void wyswietl_powtorzenia(lista **glowa)
 {
 	lista *tmp = NULL;
-	tmp = node;
+	tmp = *glowa;
 	while(tmp)
 	{
-		printf("Znak %c : %d\n", tmp->lisc->znak, tmp->lisc->ilosc);
+		if(tmp->lisc->znak == 32)
+			printf("Spacja : %d\n", tmp->lisc->ilosc);
+		else if(tmp->lisc->znak == 10)
+			printf("Znak EOL : %d\n", tmp->lisc->ilosc);
+		else
+			printf("Znak %c : %d\n", tmp->lisc->znak, tmp->lisc->ilosc);
 		tmp = tmp->next;
 	}
 	free(tmp);
 }
+
 void kodowanie_HF()
 {
 
@@ -123,12 +131,19 @@ void kodowanie_HF()
 
 int main()
 {
+	node *wezel = NULL;
+	lista *glowa = NULL; 
 	char *tekst = czytaj_plik("tekst.txt");
 	printf("%s\n", tekst);
 	int dlugosc = strlen(tekst);
-	printf("%d\n", dlugosc);
-	for(int i=0;i<dlugosc;i++)
-		stworz_lisc(tekst[0]);
-	wyswietl_powtorzenia(glowa);
+	printf("Ilosc znakow: %d\n\n", dlugosc);
+
+
+	stworz_glowe(tekst[0], &glowa);
+
+
+	for(int i=1;i<dlugosc;i++)
+		push_front_and_count(tekst[i], &glowa);
+	wyswietl_powtorzenia(&glowa);
 	return EXIT_SUCCESS;
 }
