b204e747ac38dc78fc461863cb85c2f30f256eb6
Jankels <jasio1991@gmail.com>
2018-01-16 23:13:06 +0100

    add

--- Dynamiczne_zarzadzanie_pamiecia_i_algorytmy/Kodowanie_Huffmana/functions.h
+++ Dynamiczne_zarzadzanie_pamiecia_i_algorytmy/Kodowanie_Huffmana/functions.h
@@ -14,8 +14,9 @@ typedef struct lista
 
 char *czytaj_plik(const char *plik);
 void stworz_glowe(char znak, lista **glowa);
-void push_front_and_count(char znak, lista **glowa);
+void dodanie_elementu_na_poczatek_oraz_zliczanie_powtorzen(char znak, lista **glowa);
+void wyswietl_powtorzenia(lista *glowa);
+void sortowanie_listy(lista **glowa);
 void stworz_drzewo_HF();
 void usun_drzewo_HF();
-void wyswietl_powtorzenia(lista **glowa);
 void kodowanie_HF();
--- Dynamiczne_zarzadzanie_pamiecia_i_algorytmy/Kodowanie_Huffmana/main.c
+++ Dynamiczne_zarzadzanie_pamiecia_i_algorytmy/Kodowanie_Huffmana/main.c
@@ -2,7 +2,7 @@
 	01.2018
 	Jan Komorkiewicz
 --------------------------------------------------------
-
+AAA*
 +++ Czytamy z pliku tekst.txt ktory musi znajdowac sie w tym samym katologu w ktorym uruchamiamy program.
 +++ Adres glowy przekazywany jest do funkcji jako wartosc, dzieki czemu nie musze uzywac zmiennych globalnych.
 +++ Program wczytuje tylko pierwsza linie pliku tekstowego.
@@ -16,7 +16,19 @@
 #include <string.h> //strlen, etc.
 #include "functions.h"
 
-
+/*
+////////////////////////////////////////////////////////////////////////////////////
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+------------------------------------------------------------------------------------
+TODO ---  DOKONCZYC DOKUMENTACJE U GORY AAA* , ZROBIC FUNKCJE Z TWORZENIA LISTY BBB*
+SFORMATOWAC WYSWIETLANIE WYJSCIA STD ZEBY LADNIE WYGLADALO,
+OPISAC URUCHAMIANIE ZE MAKE RUN I POTEM DODAC OBSLUGE WLASNYCH PLIKOW TEKSTOWYCH
+JESC, SPAC, KODOWAC! ;P
+JESLI CZYTA TO JAKIS PIECZON, TO KURWA DO ROBOTY!!! DZIODO I BEDALE!!!
+------------------------------------------------------------------------------------
+||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+*/
 
 int main()
 {
@@ -26,13 +38,12 @@ int main()
 	printf("%s\n", tekst);
 	int dlugosc = strlen(tekst);
 	printf("Ilosc znakow: %d\n\n", dlugosc);
-
-
 	stworz_glowe(tekst[0], &glowa);
-
-
+	// BBB*
 	for(int i=1;i<dlugosc;i++)
-		push_front_and_count(tekst[i], &glowa);
-	wyswietl_powtorzenia(&glowa);
+		dodanie_elementu_na_poczatek_oraz_zliczanie_powtorzen(tekst[i], &glowa);
+	wyswietl_powtorzenia(glowa);
+	sortowanie_listy(&glowa);
+	wyswietl_powtorzenia(glowa);
 	return EXIT_SUCCESS;
 }
